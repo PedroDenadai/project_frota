18dd80f01758a38b8c70703d3a327085c3c0ca68
PedroDenadai <93556114+PedroDenadai@users.noreply.github.com>
2023-11-07 15:37:10 -0300

    Add files via upload

---
+++ src/Tools/AdmPage.java
@@ -0,0 +1,19 @@
+package Tools;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdmPage implements ActionListener {
+    private JLabel admTitle;
+    private JButton buttomInserirNovoUsuario;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
